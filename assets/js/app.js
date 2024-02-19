4f96baf76b0e5f451d490e61ca8af43e8af07530
Danilov <nikitapro51@gmail.com>
2024-02-19 22:57:00 +0300

    init repo

---
+++ assets/js/app.js
@@ -0,0 +1,143 @@
+
+"use strict";
+// Sweet Alert CDN through JS
+let script = document.createElement("script");
+script.type = 'text/javascript';
+script.src="https://unpkg.com/sweetalert/dist/sweetalert.min.js";
+document.body.appendChild(script);
+
+// Header
+
+let header = $(`
+<nav class="navbar navbar-expand-lg fixed-top dark-theme" id="navbar">
+<a class="navbar-brand" href="index.html">
+<img src="assets/images/favicon.png" alt="" height="50" width="50">
+</a>
+<div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+  <div id="js-hamburger" class="hamburger">
+    <span class="first"></span>
+    <span class="second"></span>
+    <span class="third"></span>
+  </div>
+
+</div>
+
+<div class="collapse navbar-collapse " id="navbarSupportedContent">
+  <ul class="navbar-nav ml-auto" id = "navbar-content">
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Обо мне</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Проекты</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="works.html">Работы</a></li>
+  </ul>
+</div>
+</nav>`);
+
+// Footer
+
+let footer = $(`
+<footer class="footer sticky-bottom"  style="background-color:#2b2a2a;">
+  <div class="p-4">
+    <div class="container-fluid quote-container">
+    <a class="tag" href="/"><img src="assets/images/favicon.png" alt="" class="rounded-circle" height="50" width="50"></a>
+    </div>
+    
+
+    </div>
+</footer>
+`);
+
+//"Scroll to top" button
+let upArrow = $(`
+  <button id="btnScrollToTop" onclick="scrollToTop()"><i class="fas fa-2x fa-angle-up"></i></button>
+  <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
+  })
+`);
+
+$(document).ready(function () {
+  //function for the "Scroll To Top" button to detect the footer
+  $(window).scroll(function () {
+    //The button will be hidden until we scroll more than the window's height
+    if ($(window).scrollTop() < $(window).height()) {
+      $("#btnScrollToTop").css("visibility", "hidden");
+    } else {
+      $("#btnScrollToTop").css("visibility", "visible");
+      //The button will change it's color when it hits the footer
+      if (
+        $(window).scrollTop() + $(window).height() >
+        $(document).height() - 838
+      ) {
+        // 838 should be changed if footer's height is changed so that the button changes it's color exactly when it hits the footer (preferably 14 less than the computer height of the footer)
+        $("#btnScrollToTop").css("background-color", "#6668FF");
+      } else {
+        $("#btnScrollToTop").css("background-color", "#6668FF");
+      }
+    }
+  });
+});
+
+//function to scroll to top
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+// Window Loads
+$(function () {
+  let bodyElement = $(`body`);
+  bodyElement.prepend(header);
+  // bodyElement.append(footer);
+  bodyElement.append(upArrow);
+  $("#btnScrollToTop").css("visibility", "hidden");
+  
+
+// function for toggling hamburger is-active class
+
+$(function () {
+  $("#js-hamburger").on("click", function () {
+    $(this).toggleClass("is-active");
+  });
+});
+
+// Navbar current page highlight
+
+let loader = document.querySelector(".loader-container");
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disappear");
+}
+$(function () {
+  $("a.nav-link").each(function () {
+    if ($(this).prop("href") == window.location.href) {
+      $(this).addClass("current-link");
+    }
+  });
+});
+
+//function to remove underline on hover
+
+$(document).ready(function () {
+  $("a.nav-link").hover(
+    function () {
+      $(this).removeClass("current-link");
+    },
+    function () {
+      if ($(this).prop("href") == window.location.href) {
+        $(this).addClass("current-link");
+      }
+    }
+  );
+});
+
+// Function to remove scroll bar during preload
+$(window).on("load", function () {
+  setTimeout(function () {
+    $(".no-scroll-preload").css("overflow", "visible");
+  }, 1000);
+  $(".loader-container").fadeOut(2500);
+});
+});
